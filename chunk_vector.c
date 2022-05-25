5e76c007b9cf734d144d1916d35c705252a559df
TeodorDyakov <tedy1996@gmail.com>
2022-05-26 00:36:23 +0300

    init

---
+++ chunk_vector.c
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press Run button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int32_t* arr[31];
+int32_t sz = 0;
+int32_t index = 0;
+int32_t bucket = 0;
+
+void init(){
+    arr[bucket] = malloc((1 << bucket) * sizeof(int32_t));
+}
+
+void add(int val){
+    if(index >= 1 << bucket){
+        bucket++;
+        arr[bucket] = malloc((1 << bucket) * sizeof(int32_t));
+        index = 0;
+    }
+    arr[bucket][index] = val;
+    index++;
+    sz++;
+}
+
+int32_t get(int32_t idx){
+    //array index out of bounds check omitted
+    idx++;
+    int high = 32 - __builtin_clz(idx) - 1;
+    int low = idx - (1 << (high));
+    return arr[high][low];
+}
+    
+int main(){
+    
+    init();
+    
+    for(int i = 0; i < 100; i++){
+        add(i);
+    }
+    // arr[0][0] = 5;
+    for(int i = 0; i < 100; i++){
+        printf(%d , get(i));
+    }
+    
+}
